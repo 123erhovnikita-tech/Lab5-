60bb1291f46b1f5804545ef123bd10dddfa5b6dc
123erhovnikita-tech <123erhovnikita@gmail.com>
2026-01-15 14:46:21 +0500

    Initial commit: Hello world program

---
+++ lab.5/lab.5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void readFromFile(vector<string>& lines)
+{
+}
+
+void printToScreen(const vector <string>& lines)
+{
+
+}
+
+
+void writeToFile(const vector <string>& lines, const string& filename = "output.txt")
+{
+
+}
+
+int main()
+{
+    return 0;
+}
