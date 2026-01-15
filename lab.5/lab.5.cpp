5d5ccb6a48a4e4fd39fb17800f63c3cc23ade86b
IlyxChxliy <ilyachaily2006@gmail.com>
2026-01-15 15:19:39 +0500

    initial function

--- lab.5/lab.5.cpp
+++ lab.5/lab.5.cpp
@@ -4,22 +4,43 @@
 
 using namespace std;
 
-void readFromFile(vector<string>& lines)
+void printToScreen (const vector <string> &lines)
 {
+    if (lines.empty ())
+    {
+        wcout << L"Âåêòîð ïóñò. Íå÷åãî âûâîäèòü." << endl;
+        return;
+    }
+    wcout << L"Âûâîä ñòðîê íà ýêðàí" << endl;
+    for (size_t i = 0; i < lines.size (); i++)
+    {
+        cout << "[" << i + 1 << "] " << lines [i] << endl;
+    }
+    wcout << endl;
 }
 
-void printToScreen(const vector <string>& lines)
+void writeToFile(const vector <string> &lines, const string &filename = "output.txt")
 {
-
-}
-
-
-void writeToFile(const vector <string>& lines, const string& filename = "output.txt")
-{
-
+    ofstream outputFile(filename);
+    if (!outputFile.is_open())
+    {
+        wcout << L"Îøèáêà: íå óäàëîñü ñîçäàòü ôàéë " << filename << endl;
+        return;
+    }
+    for (const auto &line : lines)
+    {
+        outputFile << line << endl;
+    }
+    outputFile.close ();
+    wcout << L"Çàïèñàíî " << lines.size () << L" ñòðîê â ôàéë " << filename << endl;
 }
 
 int main()
 {
+    setlocale (LC_ALL "RU")
+    vector <string> lines;
+    readFromFile (lines);
+    printToScreen (lines);
+    writeToFile (lines);
     return 0;
 }
